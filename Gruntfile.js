1dae94574de04b96373d6a29b866621bfa1fc91c
Krzysiu <kheciek@gmail.com>
2017-09-06 20:57:35 +0200

    Beautified Gruntfile.js and deleted unnecessary files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,45 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-  	sass: {
-  		options: {
-  			sourceMap: true
-  		},
-  		dist: {
-  			files: {
-  				'css/main.css': 'sass/main.sass'
-  			}
-  		}
-  	},
+	// Project configuration.
+	grunt.initConfig({
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'css/main.css': 'sass/main.sass'
+				}
+				A
+			}
+		},
 
-  	imagemin: {
-  		dynamic: {
-  			files: [{
-  				expand: true,
-  				cwd: 'images/',
-  				src: ['**/*.{png,jpg,gif}'],
-  				dest: 'images/build/'
-  			}]
-  		}
-  	},
-    watch: {
-        scripts: {
-            files: ['sass/*.sass'],
-            tasks: ['sass'],
-            options: {
-                spawn: false,
-            },
-        }
-    }
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd: 'images/',
+					src: ['**/*.{png,jpg,gif}'],
+					dest: 'images/build/'
+				}]
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['sass/*.sass'],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				},
+			}
+		}
 
-  });
-  // Load the plugins tasks
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  // Default task(s).
+	});
+	// Load the plugins tasks
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	// Default task(s).
 
-  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+	grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
 };
